acb093f6c820de47630e7cfc4d56bd2d157ffb94
shanenolanwit <shane.nolan.wit@gmail.com>
2019-04-21 22:15:12 +0100

    updated fragment to use base 64 images from server

--- app/src/main/java/com/example/fragments/ListBeerFragment.java
+++ app/src/main/java/com/example/fragments/ListBeerFragment.java
@@ -34,30 +34,10 @@ public class ListBeerFragment extends Fragment {
 
     private static final String TAG = "ListBeerFragment";
 
+    private BeerRecyclerViewAdapter adapter;
+
     public ListBeerFragment() {
     }
-
-
-
-//    public View doSomething(View view) {
-//
-//
-//        RecyclerView recyclerView = view.findViewById(R.id.recyclerview);
-//        recyclerView.setLayoutManager(layoutManager);
-//        ArrayList<String> mNames = new ArrayList<>();
-//        ArrayList<String> mBase64images = new ArrayList<>();
-//        mNames.add("image 1");
-//        mNames.add("image 2");
-//        mNames.add("image 3");
-//        mNames.add("image 4");
-//        mBase64images.add("https://ui.sportsbetting.ag/nts/images/paige-vanzant-eyes-return.jpg");
-//        mBase64images.add("https://pbs.twimg.com/profile_images/1025970166260064256/XWnsJ8PZ_400x400.jpg");
-//        mBase64images.add("https://www.lowkickmma.com/wp-content/uploads/2018/09/9135772_web1_web-pvz-082616.jpg");
-//        mBase64images.add("https://www.samaa.tv/wp-content/uploads/2018/11/Paige-Vanzant-1-640x356.jpg");
-//        BeerRecyclerViewAdapter adapter = new BeerRecyclerViewAdapter(getContext(),null,null);
-//        recyclerView.setAdapter(adapter);
-//        return recyclerView;
-//    }
 
     @Nullable
     @Override
@@ -68,20 +48,24 @@ public class ListBeerFragment extends Fragment {
 
         Retrofit retrofit = MapMyBeerAPIClient.getRetrofitClient();
         MapMyBeerAPIInterface api = retrofit.create(MapMyBeerAPIInterface.class);
+        Log.d(TAG, "onCreateView: " + mBase64images.size());
+        adapter = new BeerRecyclerViewAdapter(getContext(), mNames, mBase64images);
+
         Call call = api.getBeers();
         call.enqueue(new Callback() {
             @Override
             public void onResponse(Call call, Response response) {
-                Log.d(TAG, "onResponse: Success");
-                Log.d(TAG, "onResponse: " + call);
-                Log.d(TAG, "onResponse: " + response);
-                Log.d(TAG, "onResponse: " + response.body());
+                Log.d(TAG, "ListBeerFragment onResponse: success" );
                 BeerListRetrofit bl = (BeerListRetrofit) response.body();
                 Log.d(TAG, "onResponse: " + bl.getBeers());
-                for(BeerRetrofit beer : bl.getBeers()){
-                    Log.d(TAG, "onResponse: " + beer.getName());
+                ArrayList<String> mNames = new ArrayList<>();
+                ArrayList<String> mBase64images = new ArrayList<>();
+                for (BeerRetrofit beer : bl.getBeers()) {
+                    mNames.add(beer.getName());
+                    mBase64images.add(beer.getBase64img());
                 }
-        }
+                getAdapter().update(mNames, mBase64images);
+            }
 
             @Override
             public void onFailure(Call call, Throwable t) {
@@ -90,34 +74,12 @@ public class ListBeerFragment extends Fragment {
                 t.printStackTrace();
             }
         });
-            View rootView = inflater.inflate(R.layout.beer_list, container, false);
-
-            RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerview);
-
-
-            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
-
-
-            mNames.add("image 1");
-            mNames.add("image 2");
-            mNames.add("image 3");
-            mNames.add("image 4");
-            mNames.add("image 1");
-            mNames.add("image 2");
-            mNames.add("image 3");
-            mBase64images.add("https://ui.sportsbetting.ag/nts/images/paige-vanzant-eyes-return.jpg");
-            mBase64images.add("https://pbs.twimg.com/profile_images/1025970166260064256/XWnsJ8PZ_400x400.jpg");
-            mBase64images.add("https://www.lowkickmma.com/wp-content/uploads/2018/09/9135772_web1_web-pvz-082616.jpg");
-            mBase64images.add("https://www.samaa.tv/wp-content/uploads/2018/11/Paige-Vanzant-1-640x356.jpg");
-            mBase64images.add("https://ui.sportsbetting.ag/nts/images/paige-vanzant-eyes-return.jpg");
-            mBase64images.add("https://pbs.twimg.com/profile_images/1025970166260064256/XWnsJ8PZ_400x400.jpg");
-            mBase64images.add("https://www.lowkickmma.com/wp-content/uploads/2018/09/9135772_web1_web-pvz-082616.jpg");
-            BeerRecyclerViewAdapter adapter = new BeerRecyclerViewAdapter(getContext(),mNames,mBase64images);
-            recyclerView.setAdapter(adapter);
-
-            recyclerView.setItemAnimator(new DefaultItemAnimator());
-
-            return rootView;
+        View rootView = inflater.inflate(R.layout.beer_list, container, false);
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+        recyclerView.setAdapter(adapter);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        return rootView;
     }
 
     @Override
@@ -126,7 +88,46 @@ public class ListBeerFragment extends Fragment {
         Log.d(TAG, "onViewCreated: called");
     }
 
+    private void updateData(){
+        Retrofit retrofit = MapMyBeerAPIClient.getRetrofitClient();
+        MapMyBeerAPIInterface api = retrofit.create(MapMyBeerAPIInterface.class);
 
+        Call call = api.getBeers();
+        call.enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) {
+                Log.d(TAG, "ListBeerFragment onResponse: success" );
+                BeerListRetrofit bl = (BeerListRetrofit) response.body();
+                Log.d(TAG, "onResponse: " + bl.getBeers());
+                ArrayList<String> mNames = new ArrayList<>();
+                ArrayList<String> mBase64images = new ArrayList<>();
+                for (BeerRetrofit beer : bl.getBeers()) {
+                    mNames.add(beer.getName());
+                    mBase64images.add(beer.getBase64img());
+                }
+                getAdapter().update(mNames, mBase64images);
+            }
 
+            @Override
+            public void onFailure(Call call, Throwable t) {
+                Log.d(TAG, "onFailure: Failure");
+                Log.d(TAG, "onFailure: " + t.getMessage());
+                t.printStackTrace();
+            }
+        });
+    }
 
+    // https://stackoverflow.com/questions/41655797/refresh-fragment-when-change-between-tabs
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser) {
+            Log.d(TAG, "setUserVisibleHint: refresssssssh");
+            updateData();
+        }
+    }
+
+    public BeerRecyclerViewAdapter getAdapter() {
+        return adapter;
+    }
 }
